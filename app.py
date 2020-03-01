14676bc816b3a98776b2704ef588ed5909ea7a1a
Niraj Bista <admin@karnali.com>
2020-03-01 05:05:53 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import click
 
 @click.command()
 def hello():
-    click.echo('Hello World!')
+    click.echo('Hello we will build docker locally in cloud9 then push it to docker hub with script.')
 
 if __name__ == '__main__':
     hello()
